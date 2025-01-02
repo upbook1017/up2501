96d20315e0de1b442c80a522e8e40a9957b08ef2
upbook1017 <upbook1017@gmail.com>
2025-01-03 02:36:59 +0900

    0103(投稿作成日を追加)

--- resources/views/board/index.blade.php
+++ resources/views/board/index.blade.php
@@ -8,6 +8,7 @@
             <tr>
                 <td><a href="{{ route('board.show', $item) }}">{{ $item->getData() }}</a></td><!--Boardモデルの全てのデータを出力-->
                 <td>{{ $item->posts_count }}</td><!--withCountにおいて「posts_count」と命名規則が決まっている。-->
+                <td>{{ $item->created_at->format('Y-m-d')}}</td><!--レコードの作成日において「create_at」と命名規則が決まっている。-->
             </tr>
         @endforeach
     </table>
