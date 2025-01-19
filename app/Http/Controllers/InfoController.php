b309be06b04dd21418ae05ac2d8f92fe16441cb5
upbook1017 <upbook1017@gmail.com>
2025-01-20 01:19:58 +0900

    0120(お問い合わせ(InfoController.php)のコメント追加。)

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -15,10 +15,10 @@ class InfoController extends Controller
 
     public function send(InfoRequest $request)
     {
-        $data = $request->input('informessage');
+        $data = $request->input('informessage');//入力フォームよりinformessageの値を$dataに代入。
         Mail::raw($data, function ($message) {
-            $message->to('uptestuph@gmail.com')
-                ->subject('お問い合わせ');
+            $message->to('uptestuph@gmail.com')//メールアドレスに(to)$dataの$messageを送る。
+                ->subject('お問い合わせ');//メールのsubject件名。
         });
 
         return redirect()->route('board.success');
